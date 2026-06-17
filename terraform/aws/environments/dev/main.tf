4e00b4158ec24cb3332e45f36fa56b43024e1f2b
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-06-17 12:08:33 +0300

    Add AWS dev EKS infrastructure with Terraform

---
+++ terraform/aws/environments/dev/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name     = "dev-vpc"
+  vpc_cidr = "10.0.0.0/16"
+
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  availability_zones = ["eu-central-1a", "eu-central-1b"]
+}
+
+
+
+
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name        = "dev-shopping-eks"
+  subnet_ids          = module.vpc.public_subnet_ids
+  node_subnet_ids     = module.vpc.public_subnet_ids
+  node_instance_types = ["t3.small"]
+}
