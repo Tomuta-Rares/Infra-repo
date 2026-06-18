f3b9c856f2633aa2f9aea714d7de21e85f39f829
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-06-18 15:54:48 +0300

    modified eks source

--- terraform/aws/environments/dev/main.tf
+++ terraform/aws/environments/dev/main.tf
@@ -42,7 +42,7 @@ module "vpc" {
 
 
 module "eks" {
-  source = "git::git@github.com:Tomuta-Rares/terraform-aws-modules.git//modules/eks?ref=v0.2.2"
+  source = "git::git@github.com:Tomuta-Rares/terraform-aws-modules.git//modules/eks?ref=v0.2.3"
 
   cluster_name        = "dev-shopping-eks"
   subnet_ids          = module.vpc.public_subnet_ids
