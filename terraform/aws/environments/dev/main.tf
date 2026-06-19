633d03f735900190d0eb17cd19f3e232207bf6b4
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-06-19 13:26:42 +0300

    added ingress-nginx

--- terraform/aws/environments/dev/main.tf
+++ terraform/aws/environments/dev/main.tf
@@ -90,3 +90,19 @@ resource "helm_release" "argocd" {
   ]
 }
 #####################################################################################################
+
+
+
+######## ingress-nginx ###############################################################################
+
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  depends_on = [
+    module.eks
+  ]
+}
