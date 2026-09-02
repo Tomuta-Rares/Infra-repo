d41ba96e0bc3c01cf714a29f0ffa68c6ed2d18be
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-09-02 16:23:56 +0300

    change

--- terraform/aws/environments/dev/main.tf
+++ terraform/aws/environments/dev/main.tf
@@ -8,6 +8,11 @@ terraform {
   }
 
   required_providers {
+
+    random = {
+      source = "hashicorp/random"
+    }
+
     aws = {
       source = "hashicorp/aws"
     }
@@ -181,7 +186,8 @@ resource "null_resource" "argocd_root_app" {
     helm_release.argocd,
     helm_release.cert_manager,
     null_resource.local_dev_root_ca,
-    null_resource.sealed_secrets_master_key
+    null_resource.sealed_secrets_master_key,
+    null_resource.external_secrets_store
   ]
 
   provisioner "local-exec" {
@@ -224,4 +230,40 @@ resource "aws_secretsmanager_secret" "mysql" {
   name = "dev-shopping/mysql"
 
   recovery_window_in_days = 0
+}
+
+resource "random_password" "mysql_user" {
+  length  = 24
+  special = false
+}
+
+resource "random_password" "mysql_root" {
+  length  = 24
+  special = false
+}
+
+resource "aws_secretsmanager_secret_version" "mysql" {
+  secret_id = aws_secretsmanager_secret.mysql.id
+
+  secret_string = jsonencode({
+    password     = random_password.mysql_user.result
+    rootPassword = random_password.mysql_root.result
+    databaseUrl  = "mysql+mysqlconnector://user:${random_password.mysql_user.result}@mysql.mysql.svc.cluster.local:3306/shopping"
+  })
+}
+
+
+resource "null_resource" "external_secrets_store" {
+  depends_on = [
+    helm_release.external_secrets,
+    aws_secretsmanager_secret_version.mysql
+  ]
+
+  triggers = {
+    manifest_sha = filesha256("${path.module}/../../../../platform/external-secrets/cluster-secret-store.yaml")
+  }
+
+  provisioner "local-exec" {
+    command = "KUBECONFIG_FILE=$(mktemp) && aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.aws_region} --kubeconfig $KUBECONFIG_FILE && kubectl --kubeconfig $KUBECONFIG_FILE apply -f ${path.module}/../../../../platform/external-secrets/cluster-secret-store.yaml && rm -f $KUBECONFIG_FILE"
+  }
 }
